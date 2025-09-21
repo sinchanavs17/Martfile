530dc090a10b962498c85501c52c9a221008387d
sinchanavs17 <89465388+sinchanavs17@users.noreply.github.com>
2025-09-21 13:06:12 +0530

    Add files via upload

---
+++ scr.js
@@ -0,0 +1,8 @@
+const products = [
+  { id: 1, name: "Apple", price: 120, category: "fruits", image: "https://cdn.pixabay.com/photo/2017/01/20/15/06/red-1999898_1280.jpg" },
+  { id: 2, name: "Banana", price: 60, category: "fruits", image: "https://cdn.pixabay.com/photo/2018/02/19/10/51/bananas-3161243_1280.jpg" },
+  { id: 3, name: "Carrot", price: 40, category: "vegetables", image: "https://cdn.pixabay.com/photo/2017/02/01/10/44/carrots-2025878_1280.jpg" },
+  { id: 4, name: "Milk", price: 50, category: "dairy", image: "https://cdn.pixabay.com/photo/2016/03/05/19/02/honey-1238463_1280.jpg" },
+  { id: 5, name: "Paneer", price: 80, category: "dairy", image: "https://cdn.pixabay.com/photo/2016/03/05/19/07/cheese-1238675_1280.jpg" },
+  { id: 6, name: "Potato", price: 30, category: "vegetables", image: "https://cdn.pixabay.com/photo/2015/03/26/09/39/potatoes-690213_1280.jpg" }
+];
